c2803919e0f29fefb3392ef08eea1b52f069ed19
eranda <erandasampath2000@gmail.com>
2020-12-24 06:52:09 +0530

    up

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 <html>
 
-<?php 
+<?php
 include("head.php");
 include("connect.php");
 ?>
 <body class="hold-transition skin-blue sidebar-mini">
 
-<?php 
+<?php
 
 include_once("auth.php");
 
@@ -63,7 +63,7 @@ if($r1=="Mr.Chaminda"){header("location: index2.php");}
 
       </h1>
 
-      
+
 
     </section>
 
@@ -75,9 +75,9 @@ if($r1=="Mr.Chaminda"){header("location: index2.php");}
 
 
 
-	
 
-	
+
+
 
 	<?php
 
@@ -89,14 +89,14 @@ if($r1=="Mr.Chaminda"){header("location: index2.php");}
 
 
 
-                  $date =  date("Y-m-d");					
+                  $date =  date("Y-m-d");
 $result = $db->prepare("SELECT sum(labor_cost) FROM sales WHERE action='active' AND  date='$date' ");
 				$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
 				  $labor=$row['sum(labor_cost)'];
 				}
-			
+
 
 				$result = $db->prepare("SELECT sum(profit) FROM sales WHERE action='active' AND  date='$date' ");
 					$result->bindParam(':userid', $date);
@@ -108,17 +108,17 @@ $result = $db->prepare("SELECT sum(labor_cost) FROM sales WHERE action='active' 
 $result = $db->prepare("SELECT sum(amount) FROM sales WHERE  action='active' AND  date='$date'  ");
 			$result->bindParam(':userid', $date);
                 $result->execute();
-                for($i=0; $row = $result->fetch(); $i++){				  
+                for($i=0; $row = $result->fetch(); $i++){
 				  $amount=$row['sum(amount)'];
-				}		
-		
+				}
+
 				$result = $db->prepare("SELECT sum(amount) FROM sales WHERE  action='active' AND  date='$date' AND customer_name='Unknown customer' ");
 					$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
 				  $dr_amount=$row['sum(amount)'];
 
-				}	
+				}
 
 $result = $db->prepare("SELECT sum(amount) FROM expenses_records WHERE  date = '$date' ");
 				$result->bindParam(':userid', $date);
@@ -130,29 +130,29 @@ $result = $db->prepare("SELECT sum(amount) FROM expenses_records WHERE  date = '
 
 		$month1=date("Y-m-01");
 		$month2=date("Y-m-31");
-		
+
 		$result = $db->prepare("SELECT * FROM model ");
 				$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
 				$model_name=$row['name'];
 					$model_id=$row['id'];
-				
+
 				$result1 = $db->prepare("SELECT sum(amount) FROM sales WHERE  model='$model_name' AND  date BETWEEN '$month1' AND '$month2' ");
 					$result1->bindParam(':userid', $date);
                 $result1->execute();
                 for($i=0; $row1 = $result1->fetch(); $i++){
 				  $model_amount=$row1['sum(amount)'];
 				}
-$sql = "UPDATE model 
+$sql = "UPDATE model
         SET amount=?
 		WHERE id=?";
 $q = $db->prepare($sql);
 $q->execute(array($model_amount,$model_id));
-				
+
 				}
-		
-		
+
+
 		$result1 = $db->prepare("SELECT sum(amount) FROM model ");
 					$result1->bindParam(':userid', $date);
                 $result1->execute();
@@ -164,23 +164,23 @@ $q->execute(array($model_amount,$model_id));
 				$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
-				$job_count=$row['count(id)'];	
+				$job_count=$row['count(id)'];
 				}
 
-	
+
 		//oooooooooooooooooooooooooooooo YAMAHA 2 ooooooooooooooooooooooooooooooooooooooooo
-	
+
 		include('connect2.php');
-		
+
 		$result = $db->prepare("SELECT sum(labor_cost) FROM sales WHERE action='active' AND  date='$date' ");
 				$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
 				  $labor2=$row['sum(labor_cost)'];
 				}
-		
-		
-		
+
+
+
 $result = $db->prepare("SELECT sum(profit) FROM sales WHERE action='active' AND  date='$date' ");
 				$result->bindParam(':userid', $date);
                 $result->execute();
@@ -194,14 +194,14 @@ $result = $db->prepare("SELECT sum(amount) FROM sales WHERE  action='active' AND
                 for($i=0; $row = $result->fetch(); $i++){
 				  $amount2=$row['sum(amount)'];
 
-				}		
+				}
 				$result = $db->prepare("SELECT sum(amount) FROM sales WHERE  action='active' AND  date='$date' AND customer_name='Unknown customer' ");
 					$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
 				  $dr_amount2=$row['sum(amount)'];
 
-				}	
+				}
 
 $result = $db->prepare("SELECT sum(amount) FROM expenses_records WHERE  date = '$date' ");
 				$result->bindParam(':userid', $date);
@@ -213,179 +213,80 @@ $result = $db->prepare("SELECT sum(amount) FROM expenses_records WHERE  date = '
 
 		$month1=date("Y-m-01");
 		$month2=date("Y-m-31");
-		
+
 		$result = $db->prepare("SELECT * FROM model ");
 				$result->bindParam(':userid', $date);
                 $result->execute();
                 for($i=0; $row = $result->fetch(); $i++){
 				$model_name2=$row['name'];
 				$model_id2=$row['id'];
-				
+
 				$result1 = $db->prepare("SELECT sum(amount) FROM sales WHERE  model='$model_name2' AND  date BETWEEN '$month1' AND '$month2' ");
 					$result1->bindParam(':userid', $date);
                 $result1->execute();
                 for($i=0; $row1 = $result1->fetch(); $i++){
 				  $model_amount2=$row1['sum(amount)'];
 				}
-$sql = "UPDATE model 
+$sql = "UPDATE model
         SET amount=?
 		WHERE id=?";
 $q = $db->prepare($sql);
 $q->execute(array($model_amount2,$model_id2));
-				
+
 				}
-		
-		
+
+
 		$result1 = $db->prepare("SELECT sum(amount) FROM model ");
 					$result1->bindParam(':userid', $date);
                 $result1->execute();
                 for($i=0; $row1 = $result1->fetch(); $i++){
 				  $month_amount2=$row1['sum(amount)'];
 				}
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 				date_default_timezone_set("Asia/Colombo");
 				$date=date("Y-m-d");
-		
-		
-		
-		
+
+
+
+
 			?>
-	 
+
 	 <?php     $r=$_SESSION['SESS_LAST_NAME'];
 if($r =='Cashier'){
 	?>
 <?php }
 else{
  ?>
-        
 
-	<?php 
+
+	<?php
 
 }
 
  ?>
 
 <div class="row">
-	  
-	  
 
-	
-	
 
-	
+
+
+
+
+
 	  </div>
 	<div class="row">
-		
-				<div class="col-md-12">
- <div class="box box-info">
-            <div class="box-header with-border">
-              <h3 class="box-title">Lone</h3>
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-              </div>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <div class="table-responsive">
-                <table class="table no-margin">
-                  <thead>
-                  <tr>
-                    <th >Id.</th>
-                    <th>customer name</th>
-                    <th>Amount</th>
-					  <th>Balance</th>
-					<th>Pay Day</th> 
-					 <th>Terms pay </th>
-					 <th>Terms pay total</th>
-					  <th>Terms Balence</th>
-					  <th>Date</th>
-					  <th>Next Pay Date</th>
-            
-                  </tr>
-                  </thead>
-                  <tbody>
-					  <?php include('connect.php');
-			$result = $db->prepare("SELECT * FROM lone WHERE action='0' ORDER by id DESC ");
-				$result->bindParam(':userid', $date);
-                $result->execute();
-                for($i=0; $row = $result->fetch(); $i++){
-			$loid=$row['id'];
-		    $con_date=$row['next_date'];
-$result1 = $db->prepare("SELECT * FROM lone_pay WHERE do='' and lone_id='$loid' ORDER by id DESC limit 0,1");
-				
-					$result1->bindParam(':userid', $date);
-                $result1->execute();
-                for($i=0; $row1 = $result1->fetch(); $i++){
-				  
-				  $lodate=$row1['date'];
-				}				
-	if($lodate==""){$lodate=$row['date'];}	
-	$split = explode("-", $lodate);
-   $y = $split[0];
-   $m = $split[1];
-   $d=$row['pay_day'];
-					if($m=="12"){$y=$y+1;}else{$m=$m+1;}
-	$con_date1=$y."-".$m."-".$d;
-$dateNow=date("Y-m-d");	
-$datetime1 = new DateTime($dateNow);
-$datetime2 = new DateTime($con_date);
-$interval = $datetime1->diff($datetime2);
-$rina= $interval->format('%R%a');			
-					
-if($rina<0){					
-  ?>
-                  <tr style="background-color:#FF0004">				
-				<?php }else{	  ?>
-		<tr>
-	<?php }  ?>
-                    <td><?php echo $row['id'];?></td>
-                    <td><?php echo $row['customer_name'];?></td>
-                    <td>Rs.<?php echo $row['amount'];?></td>
-				    <td>Rs.<?php echo $row['amount_due'];?></td>
-					<td><?php echo $row['pay_day'];?></td>  
-		            <td>Rs.<?php echo $row['monthly_pay'];?></td> 
-					<td><?php 
-$result1 = $db->prepare("SELECT count(amount) FROM lone_pay WHERE do='' and lone_id='$loid'");
-				
-					$result1->bindParam(':userid', $date);
-                $result1->execute();
-                for($i=0; $row1 = $result1->fetch(); $i++){
-				  
-				  $lotot=$row1['count(amount)'];
-				}	
-				echo $lotot;
-				$ba=$row['term']-$lotot;
-						?></td>
-				<td><?php echo $ba;?></td>
-			   <td><?php echo $row['date'];?></td>
-			   <td><?php echo $row['next_date'];?></td>
-                  </tr>
-                  
-                  <?php } ?>
-					  
-                  </tbody>
-					
-                </table>
-              </div>
-              <!-- /.table-responsive -->
-				
-            </div>
-	 
-	 
-	 </div></div>
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		</div>
 
       <!-- SELECT2 EXAMPLE -->
@@ -398,13 +299,13 @@ $result1 = $db->prepare("SELECT count(amount) FROM lone_pay WHERE do='' and lone
 
 
 
-		  
 
-		  
 
-		  
 
-		  
+
+
+
+
 
           <div class="chart">
 
@@ -414,33 +315,33 @@ $result1 = $db->prepare("SELECT count(amount) FROM lone_pay WHERE do='' and lone
 
 		 <!-- Main content -->
 
-		
 
-		
 
-		
 
-		  </div>
+
+
 
 		  </div>
 
-		
+		  </div>
 
-		
 
-		
 
-		
 
-		
 
-		
 
-		
 
-		
 
-	
+
+
+
+
+
+
+
+
+
+
 
   </div>
 
@@ -582,7 +483,7 @@ $result1 = $db->prepare("SELECT count(amount) FROM lone_pay WHERE do='' and lone
 
           pointHighlightStroke: "rgba(60,141,188,1)",
 
-          data: [<?php  echo $m1; ?>, <?php  echo $m2; ?>, <?php  echo $m3; ?>, <?php  echo $m4; ?>, <?php  echo $m5; ?>, <?php  echo $m6; ?>, <?php  echo $m7; ?>, <?php  echo $m8; ?>, <?php  echo $m9; ?>, <?php  echo $m10; ?>, <?php  echo $m11; ?>, <?php  echo $m12; ?>] 
+          data: [<?php  echo $m1; ?>, <?php  echo $m2; ?>, <?php  echo $m3; ?>, <?php  echo $m4; ?>, <?php  echo $m5; ?>, <?php  echo $m6; ?>, <?php  echo $m7; ?>, <?php  echo $m8; ?>, <?php  echo $m9; ?>, <?php  echo $m10; ?>, <?php  echo $m11; ?>, <?php  echo $m12; ?>]
 
         }
 
@@ -923,4 +824,3 @@ $result1 = $db->prepare("SELECT count(amount) FROM lone_pay WHERE do='' and lone
 </body>
 
 </html>
-
